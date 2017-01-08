93cd1572228051a8f0adddb3a2f4223e8bc9f6c7
Tim Korinth <t.core.int@googlemail.com>
2017-01-08 04:16:33 +0100

    Einlesen und Kategorien bestimmen hinzugefuegt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,30 @@
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Tim on 06.01.2017.
  */
 public class Main {
+
+    private static String pathToEvalAlone = Paths.get("", "eval_alone.txt").toAbsolutePath().toString();
+    private static String pathToEvalGroup = Paths.get("", "eval_group.txt").toAbsolutePath().toString();
+    private static String pathToTrainAlone = Paths.get("", "train_alone.txt").toAbsolutePath().toString();
+    private static String pathToTrainGroup = Paths.get("", "train_group.txt").toAbsolutePath().toString();
+
+    public static void main(String[] args){
+        List<double[][]> evalAlonePositions = Reader.readPositionsFromFile(pathToEvalAlone);
+        List<double[][]> evalGroupPositions = Reader.readPositionsFromFile(pathToEvalGroup);
+        List<double[][]> trainAlonePositions = Reader.readPositionsFromFile(pathToTrainAlone);
+        List<double[][]> trainGroupsPositions = Reader.readPositionsFromFile(pathToTrainGroup);
+
+
+
+
+
+    }
+
+
+
+
 }
--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,5 +1,37 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.LinkedList;
+
 /**
- * Created by Tim on 06.01.2017.
+ * Reads the files and returns the contained positions
  */
 public class Reader {
+
+    /**
+     * Reads the file at filepath and returns its Positions as a list of two-dimensional double-arrays with [Position]and x at [0], y at [1]
+     * @param filepath the path to the file to be read
+     * @return  Positions as a list of two-dimensional double-arrays
+     */
+    public static LinkedList<double[][]> readPositionsFromFile(String filepath){
+
+       LinkedList<double[][]> buffer = new LinkedList<double[][]>();
+
+       try{
+           FileReader fr = new FileReader(filepath);
+           BufferedReader br = new BufferedReader(fr);
+           for (String line = br.readLine(); line != null; line = br.readLine()){
+                String[] sep = line.split(";");
+                double[][] row = new double[sep.length][2];
+                for(int i = 0; i < sep.length; i++){
+                    String buf[] = sep[i].split(",");
+                    row[i][0] = Double.parseDouble(buf[0]);
+                    row[i][1] = Double.parseDouble(buf[1]);
+                }
+                buffer.add(row);
+           }
+       }catch (Exception e){
+            System.out.println("File not found or problem while reading");
+       }
+       return buffer;
+    }
 }
